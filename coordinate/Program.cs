3dc65fbb5085e6d884fd7fc7caebe4ce1d74b696
Fray <kozar20000@mail.ru>
2022-06-30 20:40:57 +0300

    coordinate

---
+++ coordinate/Program.cs
@@ -0,0 +1,16 @@
+﻿void coordinate (int x, int y)
+{
+    if (x > 0 && y > 0)
+    Console.WriteLine("Первая четверть");
+    else if (x < 0 && y > 0)
+    Console.WriteLine("Вторая четверть");
+    else if (x < 0 && y < 0)
+    Console.WriteLine("Третья четверть");
+    else if (x > 0 && y < 0)
+    Console.WriteLine("Четвертая четверть");
+}
+
+coordinate (3,6);
+coordinate (-3,6);
+coordinate (-3,-6);
+coordinate (3,-6);
