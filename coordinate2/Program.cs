a0861c1945bf3c9795e4d014cbd9e01602ce5128
Fray <kozar20000@mail.ru>
2022-06-30 21:01:10 +0300

    coor2

---
+++ coordinate2/Program.cs
@@ -0,0 +1,33 @@
+﻿void coordinate (int x)
+{
+    if (x == 1)
+    Console.WriteLine("В первой четверти x > 0, y > 0");
+    else if (x ==2)
+    Console.WriteLine("В второй четверти x < 0, y > 0");
+    else if (x == 3)
+    Console.WriteLine("В третьей четверти x < 0, y < 0");
+    else if (x == 4)
+    Console.WriteLine("В четвертой четверти x > 0, y < 0");
+    else
+    Console.WriteLine("Введите число от 1 до 4");
+}
+
+
+coordinate (1);
+coordinate (2);
+coordinate (3);
+coordinate (4);
+
+/*Console.WriteLine("Введите число от 1 до 4:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+ if (x == 1)
+    Console.WriteLine("В первой четверти x > 0, y > 0");
+    else if (x ==2)
+    Console.WriteLine("В второй четверти x < 0, y > 0");
+    else if (x == 3)
+    Console.WriteLine("В третьей четверти x < 0, y < 0");
+    else if (x == 4)
+    Console.WriteLine("В четвертой четверти x > 0, y < 0");
+    else
+    Console.WriteLine("Введите число от 1 до 4");*/
